d0a463236b363c2b93380a20050a7a4170b808b3
Abdulhakeem <habiodunoladele@gmail.com>
2022-09-11 14:05:53 -0700

    six

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - to run the code
+ *
+ *Return: equals 0
+ *
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - to get a size of variable
+ *
+ * Return: zeverything returns 0
+ *
+ */
+int main(void)
+{
+	printf("Size of a char: %lu", sizeof(char), " byte(s)\n";
+	printf("Size of an int: %lu", sizeof(int), " byte(s)\n";
+	printf("Size of a long int: %lu", sizeof(long), " byte(s)\n";
+	printf("Size of a long long int: %lu", sizeof(long long), " byte(s)\n";
+	printf("Size of a long float: %lu", sizeof(float), " byte(s)";
+
+	return (0);
+}
