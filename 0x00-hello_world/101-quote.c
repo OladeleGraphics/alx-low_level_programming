ba63b8e011f51b9be40e648b4bd1899b1b948b01
Abdulhakeem <habiodunoladele@gmail.com>
2022-09-12 01:16:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include<unistd.h>
 
 /**
  * main- entry level
  *
  * 
  * 
- * 
  *
- * Description: This if for the UNIX task
+ *
+ *
+ * Description: print a qoute using write function
  *
  * Return: This return 1
  */
@@ -15,8 +16,7 @@ int main(void)
 {
 
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, quo, 59);
-
 	
+	write(1, quo, 59);	
 	return (1);
 }
