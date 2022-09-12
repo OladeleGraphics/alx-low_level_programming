79ef66e8be7fe11d6bb9155e8f32fd164771a98e
Abdulhakeem <habiodunoladele@gmail.com>
2022-09-12 06:32:49 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - entry level
- *@lastDigit: is a variable used to store the last digit of n number generated
+ *@lDigit: is a variable used to store the last digit of n number generated
  *
  * Description: This lines of code will read the last digit od a number
  * and compare it with 6 and 0
@@ -13,23 +13,23 @@
 int main(void)
 {
 	int n;
-	int lastDigit;
+	int lDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastDigit = n % 10;
+	lDigit = n % 10;
 
-	if (lastDigit > 5)
+	if (lDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lDigit);
 	}
-	if (lastDigit == 0)
+	if (lDigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lDigit);
 	}
-	if (lastDigit < 6 && lastDigit != 0)
+	if (lDigit < 6 && lDigit != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lDigit);
 	}
 	return (0);
 }
