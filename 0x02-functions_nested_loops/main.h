d4c0b0bbe26d75584ad3bfc8608ca882745bf0fd
Abdulhakeem <habiodunoladele@gmail.com>
2022-09-15 17:04:57 -0700

    main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#include "_putchar.c"
+
+/**
+ * _putchar - will write a character on screen
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c); 
